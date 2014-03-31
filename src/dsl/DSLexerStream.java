495e793a96c2780b06e21d6be3b01d752275f3f1
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-03-30 19:50:22 -0700

    initial commit

---
+++ src/dsl/DSLexerStream.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package dsl;
+
+import java.io.Reader;
+
+
+/**
+ * @author nfischer3
+ *
+ */
+public class DSLexerStream{
+
+	/**
+	 * @param arg0 
+	 */
+	public DSLexerStream(Reader source) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/dsl/Token.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package dsl;
+
+/**
+ * @author nfischer3
+ *
+ */
+public class Token {
+	
+	public enum TokenType{
+		LAPREN("("), RPAREN(")"), SEMICOLON(";"), COLON(";"), COMMA(","), ADD("+"), SUBTRACT("-"), MULT("*"), MOD("%"), 
+		ASSIGN("="), RBRACE("{"), LBRACE("}"), ;
+		private String idString;
+		TokenType(String idString){
+			this.idString = idString;
+		}
+		public String getIdString(){
+			return idString;
+		}
+	}
+	
+	private TokenType tokenType;
+	private int tokenLine;
+	private int tokenColumn;
+	private String tokenLexeme;
+	
+	public Token(TokenType tokenType, int tokenLine, int tokenColumn,
+			String tokenLexeme) {
+		this.tokenType = tokenType;
+		this.tokenLine = tokenLine;
+		this.tokenColumn = tokenColumn;
+		this.tokenLexeme = tokenLexeme;
+	}
+
+	public TokenType getTokenType() {
+		return tokenType;
+	}
+
+	public int getTokenLine() {
+		return tokenLine;
+	}
+
+	public int getTokenColumn() {
+		return tokenColumn;
+	}
+
+	public String getTokenLexeme() {
+		return tokenLexeme;
+	}
+}
