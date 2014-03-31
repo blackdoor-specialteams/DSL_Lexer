8d65b2183fa4b039bdc83e5c3c12ae23aebc2047
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-03-30 20:13:03 -0700

    comments and framework

--- src/dsl/DSLexerStream.java
+++ src/dsl/DSLexerStream.java
@@ -19,5 +19,22 @@ public class DSLexerStream{
 	public DSLexerStream(Reader source) {
 		
 	}
+	
+	/**
+	 * Reads a single token. This method will block until a token is available, an I/O error occurs, or the end of the stream is reached.
+	 * @return The token read.
+	 */
+	public Token read(){
+		return null;
+	}
+	
+	/**
+	 * Skips tokens. This method will block until some tokens are available, an I/O error occurs, or the end of the stream is reached.
+	 * @param n - the number of tokens to skip.
+	 * @return the number of tokens actually skipped.
+	 */
+	public long skip(long n){
+		return 0;
+	}
 
 }
