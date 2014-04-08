82b5024b9b3154917ec038547ebcc4173db5e65e
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-04-08 11:04:03 -0700

    added support for comments

--- src/dsl/DSLexerStream.java
+++ src/dsl/DSLexerStream.java
@@ -3,9 +3,7 @@
  */
 package dsl;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +43,11 @@ public class DSLexerStream{
 		}
 		if(isWhiteChar(charAtHand)){
 			nextChar();
+			return read();
+		}
+		
+		if(charAtHand == '#'){
+			while(nextChar() != '\n' && charAtHand != '\r');
 			return read();
 		}
 		
