962a2024b9ac37ef4eccbef7dd8afb02d4d05fc0
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-04-08 10:27:47 -0700

    made command line arg for test.java

--- src/dsl/Test.java
+++ src/dsl/Test.java
@@ -9,7 +9,7 @@ import dsl.Token.TokenType;
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		DSLexerStream lexer = new DSLexerStream(new FileReader("test_src.txt"));
+		DSLexerStream lexer = new DSLexerStream(new FileReader(args[0]));
 		Token read=lexer.read();
 		while(!read.getTokenType().equals(TokenType.EOS)){
 			System.out.println(read = lexer.read());
